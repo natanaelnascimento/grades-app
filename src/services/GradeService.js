ff674fb1b903e93dd320be7b7766f0f6d2aab6e7
Natanael Nascimento <hello@natanael.dev>
2020-07-12 16:55:03 -0300

    Correçao de path no grade service

--- src/services/GradeService.js
+++ src/services/GradeService.js
@@ -1,31 +1,31 @@
 import http from '../http-common';
 
 const getAll = () => {
-  return http.get('/grade');
+  return http.get('/grades');
 };
 
 const get = (id) => {
-  return http.get(`/grade/${id}`);
+  return http.get(`/grades/${id}`);
 };
 
 const create = (data) => {
-  return http.post('/grade', data);
+  return http.post('/grades', data);
 };
 
 const update = (id, data) => {
-  return http.put(`/grade/${id}`, data);
+  return http.put(`/grades/${id}`, data);
 };
 
 const remove = (id) => {
-  return http.delete(`/grade/${id}`);
+  return http.delete(`/grades/${id}`);
 };
 
 const removeAll = () => {
-  return http.delete(`/grade`);
+  return http.delete(`/grades`);
 };
 
 const findByName = (name) => {
-  return http.get(`/grade?name=${name}`);
+  return http.get(`/grades?name=${name}`);
 };
 
 export default {
